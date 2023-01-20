1fd56d14e3c5d5a31d5d285bf4faf27f69842cc8
Antonio Carlos Soares Prudencio <antonio.prudencio@maida.health>
2023-01-20 13:25:31 -0300

    contagem de instrucoes mergeSort

--- src/main/java/org/ufpi/MergeSort.java
+++ src/main/java/org/ufpi/MergeSort.java
@@ -2,13 +2,15 @@ package org.ufpi;
 
 import java.util.Arrays;
 
+
 public class MergeSort {
     public static void main(String[] args) {
         int [] numeros = {0,9,7,4,1,19,0,3,5};
-        System.out.println(Arrays.toString(mergeSort(numeros, 0, numeros.length-1)));
+        System.out.println(mergeSort(numeros, 0, numeros.length-1));
     }
 
-    public static int[] merge(int [] numeros, int inicio, int meio, int fim){
+    public static int merge(int [] numeros, int inicio, int meio, int fim){
+        int count = 0;
         int esquerda = meio - inicio + 1;
         int direita = fim - meio;
 
@@ -27,6 +29,7 @@ public class MergeSort {
         j = 0;
         k = inicio;
         while (i < esquerda && j < direita){
+            count++;
             if (vetorEsquerda[i] <= vetorDireita[j]){
                 numeros[k] = vetorEsquerda[i];
                 i  = i + 1;
@@ -37,24 +40,31 @@ public class MergeSort {
             k = k + 1;
         }
         while (i < esquerda){
+            count++;
             numeros[k] = vetorEsquerda[i];
             i = i + 1;
             k = k + 1;
         }
         while (j < direita){
+            count++;
             numeros[k] = vetorDireita[j];
             j = j + 1;
             k = k + 1;
         }
-        return numeros;
+
+        return count;
     }
-    public static int[] mergeSort(int [] numeros, int inicio, int fim){
+    public static int mergeSort(int [] numeros, int inicio, int fim){
+        int count = 0;
         if(inicio >= fim){
-            return numeros;
+            return count;
+        }else {
+            int meio = (inicio+fim)/2;
+            count += mergeSort(numeros, inicio, meio);
+            count += mergeSort(numeros, meio+1, fim);
+            count += merge(numeros, inicio, meio, fim);
         }
-        int meio = (inicio+fim)/2;
-        mergeSort(numeros, inicio, meio);
-        mergeSort(numeros, meio+1, fim);
-        return merge(numeros, inicio, meio, fim);
+        System.out.println(Arrays.toString(numeros));
+        return count;
     }
 }
