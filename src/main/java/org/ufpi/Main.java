00e4fefa6aea500f574065c207f14036de90fe94
Antonio Carlos Soares Prudencio <antonio.prudencio@maida.health>
2023-01-22 20:45:22 -0300

    refactor

--- src/main/java/org/ufpi/Main.java
+++ src/main/java/org/ufpi/Main.java
@@ -4,10 +4,13 @@ import org.ufpi.domain.enums.AlgoritmoEnum;
 
 public class Main {
     public static void main(String[] args) {
-        RunAlgoritmo.executa(500,3, AlgoritmoEnum.BUBBLESORT);
-        RunAlgoritmo.executa(500,3, AlgoritmoEnum.HEAPSORT);
-        RunAlgoritmo.executa(500,3, AlgoritmoEnum.INSERTIONSORT);
-        RunAlgoritmo.executa(500,3, AlgoritmoEnum.QUICKSORT);
-        RunAlgoritmo.executa(500,3, AlgoritmoEnum.MERGESORT);
+
+        int TAM = 30000;
+        int REPT = 3;
+        RunAlgoritmo.executa(TAM,REPT, AlgoritmoEnum.BUBBLESORT);
+        RunAlgoritmo.executa(TAM,REPT, AlgoritmoEnum.HEAPSORT);
+        RunAlgoritmo.executa(TAM,REPT, AlgoritmoEnum.INSERTIONSORT);
+        RunAlgoritmo.executa(TAM,REPT, AlgoritmoEnum.QUICKSORT);
+        RunAlgoritmo.executa(TAM,REPT, AlgoritmoEnum.MERGESORT);
     }
 }
